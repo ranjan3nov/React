067befc65ba3a1bc2dd09d4135ff189b4063cf8a
ranjan3nov <ranjan3nov@gmail.com>
2024-01-20 20:58:47 +0530

    Update-20-jan

--- Fundamentals/13_megaBlogAppwrite/src/App.jsx
+++ Fundamentals/13_megaBlogAppwrite/src/App.jsx
@@ -1,15 +1,43 @@
 
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from './components'
 
 function App() {
 
-  console.log();
+  const { loading, setLoading } = useState(true);
 
-  console.log()
+  const dispatch = useDispatch();
 
-  return (
-    <> A Blog App</>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-fill block"></div>
+      <Header />
+      <main>
+        TODO:<Outlet />
+      </main>
+      <Footer />
+      test
+    </div>
+  ) : null
+
 }
 
 export default App
